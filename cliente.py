d2a550a85723da404b78d8d4ca1f16baee023743
Joana Elise <112410414+joanaeliseal@users.noreply.github.com>
2023-12-18 21:36:38 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,64 +1,147 @@
 #!/usr/bin/env python3
 
-import sys
 import socket
-import curses
-import multiprocessing
-from threading import Thread
+import os
+import sys
 
-HOST = '127.0.0.1'
-PORT = 8000
+MAX_MESSAGE_SIZE = 1024
+HOST = 'localhost'
+PORT = 8888
 
-def msg_receive(stdscr, input_box):
-    lasty, lastx = stdscr.getmaxyx() 
-    msg_box = curses.newwin(lasty-2,lastx,0,0)
-    msg_box.scrollok(True)
-    while True:
-        msg, serv = sock.recvfrom(1024)
-        if not serv or serv[0] == 0: 
-            # Thread main solicitou encerramento
-            break
-        lock.acquire()
-        try:
-            msg_box.addstr(f'Recebido> {msg.decode()}\n')
-            msg_box.refresh()
-            input_box.refresh()
-        finally:
-           lock.release()
-
-def main(stdscr):
-    stdscr.clear()
-    curses.echo()
-    lasty, lastx = stdscr.getmaxyx() 
-    input_box = curses.newwin(2,lastx,lasty-2,0)    
-    Thread(target=msg_receive,args=(stdscr, input_box)).start()
-    while True:
-        lock.acquire()
-        try:
-            input_box.clear()
-            input_box.addstr('Enviar> ')
-            input_box.refresh()
-        finally:
-            lock.release()
-        try:
-            msg = input_box.getstr().decode()
-            if msg.lower() == 'sair':
-                try:
-                    # Solicita fechamento do socket
-                    sock.shutdown(socket.SHUT_RDWR)
-                    sock.close()
-                except: pass
-                break
-            sock.sendto(msg.encode(), servidor)
-        except: pass
-
-if len(sys.argv) > 1:
+if len(sys.argv) == 2:
     HOST = sys.argv[1]
+elif len(sys.argv) == 3:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
 
-cliente  = ('0.0.0.0', 0)
-servidor = (HOST, PORT)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(cliente)
-lock = multiprocessing.Lock()
+CODIGOS_SERVIDOR = {
+    '200': 'Pedido registrado com sucesso!',
+    '201': 'Pedido encontrado com sucesso!',
+    '202': 'Item adicionado ao pedido!',
+    '203': 'Itens disponíveis listados.',
+    '204': 'Pedido finalizado com sucesso!',
+    '205': 'Cardápio esgotado.',
+    '206': 'Ainda há itens disponíveis.',
+    '207': 'Pedido entregue com sucesso!',
+    '208': 'Itens no pedido até agora.',
+    '400': 'Item inválido.',
+    '401': 'Item não está disponível!',
+    '402': 'Ainda não é possível entregar o pedido.'
+}
 
-curses.wrapper(main)
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear') # função para limpar tela do console
+
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+
+    mensagem_servidor = client_socket.recv(MAX_MESSAGE_SIZE).decode()
+    print(mensagem_servidor)
+
+    def enviar_mensagem(mensagem):
+        client_socket.send(mensagem.encode())
+        return client_socket.recv(MAX_MESSAGE_SIZE).decode()
+
+    resposta = enviar_mensagem(f"REGISTRAR {input_cliente()}")
+    print(CODIGOS_SERVIDOR[resposta])
+    resposta = enviar_mensagem("ESGOTOU")
+
+    verifica_se_esgotou(resposta)
+
+    while True:
+        choice = mostrar_menu()
+
+        if choice == '1':
+            cls()
+            if not verifica_se_esgotou(enviar_mensagem("ESGOTOU")):
+                adicionar_item_pedido(enviar_mensagem)
+
+        elif choice == '2':
+            cls()
+            if not verifica_se_esgotou(enviar_mensagem("ESGOTOU")):
+                _, resposta = enviar_mensagem("DISPONIVEIS").split("-", 1)
+                print(f'Itens disponíveis: {resposta}')
+
+        elif choice == '3':
+            cls()
+            codigo, resposta = enviar_mensagem(f"ITENS_PEDIDO").split("-", 1)
+            print(CODIGOS_SERVIDOR[codigo])
+            print(resposta)
+
+        elif choice == '4':
+            cls()
+            resposta = enviar_mensagem(f"ENTREGAR_PEDIDO")
+            if resposta == "402":
+                print(CODIGOS_SERVIDOR['402'])
+            else:
+                _, entrega = resposta.split("-", 1)
+                print(f'\nEntrega realizada!\n\nItens entregues: {entrega}\n')
+
+        elif choice == '5':
+            resposta = enviar_mensagem("SAIR")
+            if resposta == "204":
+                break
+
+        else:
+            cls()
+            print("Opção inválida! Tente novamente.")
+
+        enviar_mensagem(f"ESGOTOU")
+
+    client_socket.close()
+
+
+def adicionar_item_pedido(enviar_mensagem):
+    codigo, resposta = enviar_mensagem("DISPONIVEIS").split("-", 1)
+    if codigo == "203":
+        print("Itens disponíveis: " + resposta)
+    item = input_item()
+    if item != '':
+        codigo = enviar_mensagem(f"ADICIONAR_ITEM {item}")
+        print(CODIGOS_SERVIDOR[codigo])
+        if codigo == "202":
+            verifica_se_esgotou(enviar_mensagem(f"ESGOTOU"))
+    else:
+        print('Item inválido')
+
+
+def input_item():
+    while True:
+        item = input("Digite o item desejado: ")
+        if item.isalpha():
+            cls()
+            return item
+        else:
+            print("Entrada inválida. Digite apenas letras.")
+
+
+def input_cliente():
+    cliente = input("Digite o nome do cliente: ")
+    return cliente
+
+
+def verifica_se_esgotou(resposta):
+    if resposta == "205":
+        print("Cardápio esgotado, o pedido não pode ser entregue")
+        return True
+    else:
+        return False
+
+
+def mostrar_menu():
+    print('''
+    1 - Adicionar item ao pedido
+    2 - Ver itens disponíveis
+    3 - Ver itens no pedido
+    4 - Entregar pedido
+    5 - Sair  
+    ''')
+    choice = input("Digite a opção desejada: ")
+    return choice
+
+
+if __name__ == '__main__':
+    main()
