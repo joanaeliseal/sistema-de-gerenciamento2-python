231ad51ba4fa80fd6d37933d1dc4e74985cf5f01
Joana Elise <112410414+joanaeliseal@users.noreply.github.com>
2023-12-18 21:52:08 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -5,14 +5,14 @@ import os
 import sys
 
 MAX_MESSAGE_SIZE = 1024
-HOST = 'localhost'
-PORT = 8888
+SERVIDOR_HOST = 'localhost'
+SERVIDOR_PORTA = 8888
 
 if len(sys.argv) == 2:
-    HOST = sys.argv[1]
+    SERVIDOR_HOST = sys.argv[1]
 elif len(sys.argv) == 3:
-    HOST = sys.argv[1]
-    PORT = int(sys.argv[2])
+    SERVIDOR_HOST = sys.argv[1]
+    SERVIDOR_PORTA = int(sys.argv[2])
 
 CODIGOS_SERVIDOR = {
     '200': 'Pedido registrado com sucesso!',
@@ -29,14 +29,12 @@ CODIGOS_SERVIDOR = {
     '402': 'Ainda não é possível entregar o pedido.'
 }
 
-
-def cls():
+def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear') # função para limpar tela do console
-
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
+    client_socket.connect((SERVIDOR_HOST, SERVIDOR_PORTA))
 
     mensagem_servidor = client_socket.recv(MAX_MESSAGE_SIZE).decode()
     print(mensagem_servidor)
@@ -49,30 +47,30 @@ def main():
     print(CODIGOS_SERVIDOR[resposta])
     resposta = enviar_mensagem("ESGOTOU")
 
-    verifica_se_esgotou(resposta)
+    cardapio_esgotado(resposta)
 
     while True:
         choice = mostrar_menu()
 
         if choice == '1':
-            cls()
-            if not verifica_se_esgotou(enviar_mensagem("ESGOTOU")):
+            limpar_tela()
+            if not cardapio_esgotado(enviar_mensagem("ESGOTOU")):
                 adicionar_item_pedido(enviar_mensagem)
 
         elif choice == '2':
-            cls()
-            if not verifica_se_esgotou(enviar_mensagem("ESGOTOU")):
+            limpar_tela()
+            if not cardapio_esgotado(enviar_mensagem("ESGOTOU")):
                 _, resposta = enviar_mensagem("DISPONIVEIS").split("-", 1)
                 print(f'Itens disponíveis: {resposta}')
 
         elif choice == '3':
-            cls()
+            limpar_tela()
             codigo, resposta = enviar_mensagem(f"ITENS_PEDIDO").split("-", 1)
             print(CODIGOS_SERVIDOR[codigo])
             print(resposta)
 
         elif choice == '4':
-            cls()
+            limpar_tela()
             resposta = enviar_mensagem(f"ENTREGAR_PEDIDO")
             if resposta == "402":
                 print(CODIGOS_SERVIDOR['402'])
@@ -86,13 +84,12 @@ def main():
                 break
 
         else:
-            cls()
+            limpar_tela()
             print("Opção inválida! Tente novamente.")
 
         enviar_mensagem(f"ESGOTOU")
 
     client_socket.close()
-
 
 def adicionar_item_pedido(enviar_mensagem):
     codigo, resposta = enviar_mensagem("DISPONIVEIS").split("-", 1)
@@ -103,33 +100,29 @@ def adicionar_item_pedido(enviar_mensagem):
         codigo = enviar_mensagem(f"ADICIONAR_ITEM {item}")
         print(CODIGOS_SERVIDOR[codigo])
         if codigo == "202":
-            verifica_se_esgotou(enviar_mensagem(f"ESGOTOU"))
+            cardapio_esgotado(enviar_mensagem(f"ESGOTOU"))
     else:
         print('Item inválido')
-
 
 def input_item():
     while True:
         item = input("Digite o item desejado: ")
         if item.isalpha():
-            cls()
+            limpar_tela()
             return item
         else:
             print("Entrada inválida. Digite apenas letras.")
-
 
 def input_cliente():
     cliente = input("Digite o nome do cliente: ")
     return cliente
 
-
-def verifica_se_esgotou(resposta):
+def cardapio_esgotado(resposta):
     if resposta == "205":
         print("Cardápio esgotado, o pedido não pode ser entregue")
         return True
     else:
         return False
-
 
 def mostrar_menu():
     print('''
@@ -141,7 +134,6 @@ def mostrar_menu():
     ''')
     choice = input("Digite a opção desejada: ")
     return choice
-
 
 if __name__ == '__main__':
     main()
